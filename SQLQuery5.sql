5f426a4766f5ce46f8500eb51cc6cac86c72d573
Astridmoralesm <astrid.morales15422@est.uca.edu.ni>
2023-05-10 18:32:13 -0600

    Modificacion de tablas

--- SQLQuery5.sql
+++ SQLQuery5.sql
@@ -1,3 +1,5 @@
+update Estudiantes set FechaNac = '24/05/2003' where ID_estudiante= 4
+
 --Modificando tabla monitores
 
 select * from Monitores
@@ -16,3 +18,58 @@ delete from Clases where ID_clase = '3'
 delete from Clases where ID_clase = '8'
 delete from Clases where ID_clase = '5'
 delete from Clases where ID_clase = '10'
+
+-- Mostrando los monitores de la carrera de Ingenieria en sistemas
+
+select m.ID_monitor, e.Carrera 
+from Estudiantes e, Monitores m where (m.ID_estudiante =  e.ID_estudiante AND e.Carrera = 'Ingenieria en sistemas')
+
+-- Mostrando el nombre completo de los monitores, la clase que van a impartir y su correo electrónico
+
+select m.ID_monitor, e.Nombre, e.Apellido, c.Nombre_clase, e.Correo_electronico
+from Estudiantes e, Monitores m, Clases c where (m.ID_estudiante = e.ID_estudiante AND m.ID_clase = c.ID_clase)
+
+-- Mostrando los monitores que cumplen años en el mes de mayo
+
+select e.Nombre, e.FechaNac from Estudiantes e, Monitores m where m.ID_estudiante = e.ID_estudiante
+AND DATEPART(MONTH,FechaNac) = 05
+
+-- Monitores que cumplen años durante la semana del 8 al 13 de mayo
+
+select e.Nombre, e.FechaNac 
+from Estudiantes e, Monitores m where m.ID_estudiante = e.ID_estudiante
+AND (DATEPART(MONTH,FechaNac) = 05 AND DATEPART(DAY,FechaNac) BETWEEN 08 AND 13)
+
+-- Monitores que posean en su nombre la letra u
+ select e.Nombre
+ from Estudiantes e, Monitores m where m.ID_estudiante = e.ID_estudiante
+ AND e.Nombre LIKE '%u%'
+
+ -- Monitores por una clase especifica
+
+ select e.Nombre, c.Nombre_clase
+ from Estudiantes e, Monitores m, Clases c where (m.ID_estudiante = e.ID_estudiante AND m.ID_clase = c.ID_clase)
+ AND c.Nombre_clase = 'Calculo'
+
+ -- Monitores Contar cuantos monitores hay mayores de edad.
+
+ Select --*, DATEDIFF(YEAR,e.FechaNac,GETDATE()) as Edad 
+		 SUM (CASE WHEN DATEDIFF(YEAR,e.FechaNac,GETDATE()) > 18 
+		THEN 1 ELSE 0 END) AS 'Mayor'
+ from Estudiantes e, Monitores m where m.ID_estudiante = e.ID_estudiante 
+
+ -- Contar cuantos monitores son menores de edad
+ Select --*, DATEDIFF(YEAR,e.FechaNac,GETDATE()) as Edad 
+		 SUM (CASE WHEN DATEDIFF(YEAR,e.FechaNac,GETDATE()) < 18 
+		THEN 1 ELSE 0 END) AS 'Menor'
+ from Estudiantes e, Monitores m where m.ID_estudiante = e.ID_estudiante 
+
+ -- Horarios de clases 
+ select Horario from Clases 
+
+-- Campo clases
+Alter table Clases add Activo bit default 'true'
+
+drop table Monitores
+drop table Estudiantes
+drop database BDMonitores
